3c61e905d61035da15e2021efc16e01a7e70632c
amitkumariitm <amitkumarsumit62027@gmail.com>
2024-10-08 17:25:44 +0530

    added venv

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
 # initialize the first python file
+
+
+from typing import Union
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+async def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
